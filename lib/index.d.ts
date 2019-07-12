54761bd52084b745ba35605dcc593766423b228b
Nazim Lachter <nazim@blueforest.fr>
2019-07-12 19:01:12 +0200

    No more lib/

--- lib/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { RecipeId, DocumentId, CollectionId, Lines } from "./types";
-export default class Recipes {
-    private static API;
-    static enableLocalMode(port?: number): void;
-    private static request;
-    static getDocument(recipe: RecipeId, document: DocumentId): Promise<any>;
-    static getLines(recipe: RecipeId, collection: CollectionId, filters?: any): Promise<Lines>;
-}
--- lib/types.d.ts
+++
@@ -1,6 +0,0 @@
-export declare type RecipeId = string;
-export declare type CollectionId = string;
-export declare type DocumentId = string;
-export declare type LineId = string;
-export declare type Line = any;
-export declare type Lines = Line[];
