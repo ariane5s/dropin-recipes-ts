a7aade21e82c6bc25688385b76b23c01a6cccc2f
Jeremy Levy <jeremy.levy@zei-world.com>
2019-07-29 18:28:38 +0200

    Ajout de l'output notification

---
+++ src/outputs/Notification.ts
@@ -0,0 +1,18 @@
+import { Output } from "../core/Output"
+
+export type NotificationId = string
+
+export interface NotificationAuthor {
+  id: string,
+  firstName?: string,
+  lastName?: string,
+}
+
+export interface NotificationData {
+  content: string
+  author?: NotificationAuthor
+  isRead: boolean
+  user: string
+}
+
+export type Notification = Output<NotificationId> & NotificationData
